285d9b140222d2dd30779b5e9de64d5888246126
senninsugar <hiroki20134@gmail.com>
2026-05-23 07:18:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteSingleFile()
+  ],
 
   server: {
     host: '0.0.0.0',
     port: 5173,
   },
+
+  build: {
+    target: 'esnext'
+  }
 })
