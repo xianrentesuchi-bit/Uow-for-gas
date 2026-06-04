b251432213ecd937769fac7dc71e66a89766a9aa
senninsugar <hiroki20134@gmail.com>
2026-06-04 12:22:56 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,58 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     vue(),
-    viteSingleFile()
+
+    viteSingleFile(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'Tube',
+        short_name: 'Tube',
+        description: 'Tube PWA',
+
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+
+        display: 'standalone',
+
+        start_url: './',
+
+        scope: './',
+
+        icons: [
+          {
+            src: 'android-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
 
   base: './',
 
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 5173
   },
 
   build: {
@@ -34,7 +74,7 @@ export default defineConfig({
 
         chunkFileNames: 'index.js',
 
-        assetFileNames: 'index.[ext]',
+        assetFileNames: 'index.[ext]'
       }
     }
   }
